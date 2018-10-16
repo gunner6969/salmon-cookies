ea318c300b7170d27936e4c84cf0b66a127b6bb1
Dylan Gunn <dylgunn@wl.k12.ia.us>
2018-10-16 14:55:54 -0500

    Added products and filters

---
+++ js/products-constructor.js
@@ -0,0 +1,30 @@
+'use strict'
+
+    var product_list = [];
+   
+    var Product = function(product){
+        this.image = product.image;
+        this.title = product.title;
+        this.price = product.price;
+        this.sizes = product.sizes;
+        this.description = product.description;
+        this.colors = product.colors;
+        this.materials= product.materials;
+        this.brand = product.brand;
+        this.categories = product.categories;
+        this.product_id = product.product_id;
+    };  
+    
+    
+    Product.prototype.toHtml = function(){
+        var source = $('#product-template').text();
+        var template = Handlebars.compile(source);
+        return template(this);
+     };
+
+     product_list.forEach(function(product) {
+        if(product.title.toLowerCase().indexOf(lookup) >= 0
+     
+     product_list.forEach(function(product){
+        $('#product-listings').append(product.toHtml());
+    });
---
+++ js/products.js
@@ -0,0 +1,197 @@
+var products = [
+{
+        image:"Galaxy-cleats.jpg",
+        title: "Galaxy Football Cleats",
+        price: 150.73,
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy football cleats",
+        materials: ["30% plastic", "30"],
+        brand: "Nike",
+        product_id: "C1900"  
+    },
+    {
+        
+        image:"nike-sb-zoom-stefan-janoski-canvas-mexico-city.jpgM I",
+        title: "Nike SB Zoom Stefan Janoski tie-on",
+        price: "$85",
+        sizes: [ "9","10", "11", "12", "12.5"],
+        description: "The Nike SB Zoom Stefan Janoski Slip-On Men's Skateboarding Shoe features a built-in Nike Zoom Air unit in the sockliner and a flexible rubber outsole for responsive cushioning and exceptional boardfeel.",
+        materials: [" 90% cotton", "30"],
+        brand: "Nike",
+        product_id: "833563-012"  
+    },
+  
+ 
+    {
+        image:"Air-Jordan-12-Retro-OVO-White.jpg",
+        title: "Air Jordon 12 Retro OVO",
+        price: "$200",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "The Air Jordan 12 Retro Premium Men's Shoe celebrates the original with a premium combination upper and full-length Nike Zoom Air cushioning for elevated comfort.",
+        materials: ["45% leather plastic", "30"],
+        brand: "Nike",
+        product_id: "BV8016-445"  
+    },
+    {
+        image:"gucci.jpg",
+        title: "Gucci Slides",
+        price: "$210",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "White rubber with green and red Web",
+        materials: ["30% plastic", "30"],
+        brand: "Gucci",
+        product_id: "C1904"  
+    },
+    {
+        image:"",
+        title: "Adidas NMD Anarchy",
+        price: "$180.32",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "NMD Nation",
+        materials: ["30% plastic", "30"],
+        brand: "Adidas",
+        product_id: "C1905"  
+    },
+    {
+        image:"Walmart Retros 2.0.jpg",
+        title: "Skethers",
+        price: "$5.00",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Sketchers",
+        materials: ["90% Cotton", "30"],
+        brand: "Walmart",
+        product_id: "429469"  
+    },
+    {
+        image:"Spidy.jpg",
+        title: "Spiderman Sketchers",
+        price: "$67.00",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Skethchers Spidermans",
+        materials: ["30% plastic", "30"],
+        brand: "Walmart",
+        product_id: "C1907"  
+    },
+    {
+        image:"Adidas nmd.jpg",
+        title: "NMD Human Race",
+        price: "$550.86",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Human Race",
+        materials: ["30% plastic", "30"],
+        brand: "Adidas",
+        product_id: "C1908"  
+    },
+    {
+        image:"Most expensive.jpg",
+        title: "Balenciaga",
+        price: "$857.75",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "Balenciaga",
+        materials: ["30% Cotton", "30"],
+        brand: "Balenciaga Gang",
+        product_id: "C1909"  
+    },
+    {
+        image:"Yezzi.jpg",
+        title: "Yezzy",
+        price: "$465.54",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "Boss",
+        materials: ["30% cotton", "30"],
+        brand: "yezzy",
+        product_id: "C1910"  
+    },
+    {
+        image:"off whie NMD.jpg",
+        title: "Off White Urban NMD",
+        price:" $450.43",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is the NMD",
+        materials: ["30% cotton", "30"],
+        brand: "Adidas",
+        product_id: "C1911"  
+    },
+    {
+        image:"offwhitenikeairforce-1jpg",
+        title: "Off white boss",
+        price: "$345.65",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy ",
+        materials: ["30% plastic", "30"],
+        brand: "Nike",
+        product_id: "C1912"  
+    },
+    {
+        image:"off white.jpg",
+        title: "Off White Nike Air Vapormax",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy ",
+        materials: ["30% plastic", "30"],
+        brand: "Nike",
+        product_id: "C1913"  
+    },
+    {
+        image:"force.jpg",
+        title: "Air force One",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy ",
+        materials: ["30% leather", "30"],
+        brand: "Nike",
+        product_id: "C1914"  
+    },
+    {
+        image:"off-white-nike-air-force-1-complex-con-exclusive-5",
+        title: "Off white Air force ones",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy ",
+        aterials: ["30% plastic", "30"],
+        rand: "Nike",
+        roduct_id: "C1915"  
+    },
+    {
+        image:"Adidas.jpg",
+        title: "Adidas NMD",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy ",
+        materials: ["30% plastic", "30"],
+        brand: "Adidas",
+        product_id: "C1916"  
+    },
+    {
+        image:"vapormax.jpg",
+        title: "Nike Air Vapormax",
+        price:  "$150.73",
+        size: [ "9", "9.5", "10", "11", "12", "12.5"],
+        sescription: "This is Galaxy ",
+        materials: ["30% plastic", "30"],
+        brand: "Nike",
+        product_id: "C1917"  
+    },
+    {
+        image:"MOST-EXPENSIVE-SNEAKERS-13",
+        title: "Human Race NMD",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy football cleats",
+        materials: ["30% plastic", "30"],
+        brand: "Adidas",
+        product_id: "C1918"  
+    },
+    {  
+        image:"static.jpg",
+        title: "Static Nike",
+        price: "$150.73",
+        sizes: [ "9", "9.5", "10", "11", "12", "12.5"],
+        description: "This is Galaxy",
+        materials: ["30% plastic", "30"],
+        brand: "Nike",
+        product_id: "C1919"  
+    },
+];
+
+
